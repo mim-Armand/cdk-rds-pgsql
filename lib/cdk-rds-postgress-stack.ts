bdfa966ae561ab72126d7a7f1909e186f8f345cc
mim <mim3dot@gmail.com>
2023-09-26 21:13:27 -0500

    Networking and security tweaks: - VPC endpoint for Glue - Self referencing security group - Defined subnets - more bs

--- lib/cdk-rds-postgress-stack.ts
+++ lib/cdk-rds-postgress-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import {Construct} from 'constructs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import {SubnetType} from 'aws-cdk-lib/aws-ec2';
 import * as rds from 'aws-cdk-lib/aws-rds';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
@@ -18,7 +19,23 @@ export class CdkRdsPgdslStack extends cdk.Stack {
         const dbName = 'my_initial_database'; // Database name for the RDS instance,
         const vpcName = 'Athena-POC-VPC'; // VPC name
         const secretName = 'rds-db-secrets'; // The secret name that can be used as secret name prefix to obtain the secret in other systems
-        const vpc = new ec2.Vpc(this, vpcName, {maxAzs: 2}); // Create a new VPC with a maximum of 2 availability zones
+        const vpc = new ec2.Vpc(this, vpcName, {
+            maxAzs: 2,
+            natGateways: 1, // Indicates the number of NAT gateways to create. Set to 1 for simplicity.
+            subnetConfiguration:[{
+                cidrMask: 24,
+                name: 'ingress',
+                subnetType: SubnetType.PUBLIC,
+            },{
+                cidrMask: 24,
+                name: 'compute',
+                subnetType: SubnetType.PRIVATE_WITH_EGRESS,
+            },{
+                cidrMask: 28,
+                name: 'rds',
+                subnetType: SubnetType.PRIVATE_ISOLATED,
+            }]
+        }); // Create a new VPC with a maximum of 2 availability zones
         const rdsRole = new iam.Role(this, 'RDSRole', { // Create a new IAM role that can be assumed by the RDS service
             assumedBy: new iam.ServicePrincipal('rds.amazonaws.com'),
         });
@@ -43,22 +60,28 @@ export class CdkRdsPgdslStack extends cdk.Stack {
         });
         // Create a new RDS instance
         const dbInstance = new rds.DatabaseInstance(this, 'DatabaseInstance', {
-            engine: rds.DatabaseInstanceEngine.postgres({version: rds.PostgresEngineVersion.VER_15_3}),
+            engine: rds.DatabaseInstanceEngine.postgres({version: rds.PostgresEngineVersion.VER_14}), // Glue/JDBC doesn't support higher versions as of now. :(
             instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE3, ec2.InstanceSize.MICRO),
             vpc,
-            vpcSubnets: {subnetType: ec2.SubnetType.PUBLIC},
+            vpcSubnets: {
+                subnetType: ec2.SubnetType.PUBLIC,
+                onePerAz: true, // at most one subnet per AZ
+            },
             publiclyAccessible: true,
             allocatedStorage: 15,
+            instanceIdentifier: 'cdk-rds-postgres-01',
             backupRetention: cdk.Duration.days(3),
             removalPolicy: cdk.RemovalPolicy.DESTROY,
             databaseName: dbName, // database name or use the default value which would be postgres in this case
             credentials: rds.Credentials.fromSecret(dbSecret), // Use the secret created above for the database credentials
         });
         dbInstance.grantConnect(rdsRole, "postgresadmin");
+        dbInstance.connections.allowDefaultPortInternally("attempting to allow the lambda in the same SG to connect");
         // Retrieve the security group of the RDS instance and add an ingress rule to allow traffic on port 5432
         const securityGroup = ec2.SecurityGroup.fromSecurityGroupId(this, 'DatabaseSecurityGroup', dbInstance.connections.securityGroups[0].securityGroupId);
         // securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(5432), 'Allow all IPv4 traffic on port 5432 just for demo/temporary usecases');
-        securityGroup.addIngressRule(ec2.Peer.ipv4(vpc.vpcCidrBlock), ec2.Port.allTraffic(), 'Allow all traffic from Lambda function within the VPC');
+        securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.allTraffic(), 'Allow all traffic from Lambda function within the VPC');
+        securityGroup.addIngressRule(securityGroup, ec2.Port.allTraffic(), 'Self-Referencing Security group (for Glue stuff)')
         // Get endpoint and port details of the RDS instance
         const dbInstanceEndpointAddress = dbInstance.dbInstanceEndpointAddress;
         const dbInstanceEndpointPort = dbInstance.dbInstanceEndpointPort;
@@ -67,19 +90,34 @@ export class CdkRdsPgdslStack extends cdk.Stack {
         // vpc.addInterfaceEndpoint('lambdaEndpoint', {});
         const vpcEndpoint = new ec2.InterfaceVpcEndpoint(this, 'EndpointService', {
             service: ec2.InterfaceVpcEndpointAwsService.LAMBDA,
+            open: true, // allow VPC traffic to the endpoint.
             vpc,
-            privateDnsEnabled: false,
+            privateDnsEnabled: true,
             securityGroups: [securityGroup]
         });
+
+        new ec2.InterfaceVpcEndpoint(this, 'EndpointService2', {
+            service: ec2.InterfaceVpcEndpointAwsService.GLUE,
+            open: true,
+            vpc,
+            privateDnsEnabled: true,
+            securityGroups: [securityGroup]
+        })
 
         // Output various details about the created resources as CloudFormation outputs, you can add and remove as you need
         new cdk.CfnOutput(this, 'AthenaVpcIdOutput', {
             value: vpc.vpcId,
+            exportName: 'AthenaVpcIdOutput'
         });
         new cdk.CfnOutput(this, 'AthenaVpcPrivateSubnetsOutput', {
             // value: JSON.stringify(vpc.privateSubnets.map(subnet => subnet.subnetId)),
             value: vpc.privateSubnets.map(subnet => subnet.subnetId).join(','),
             exportName: 'AthenaVpcPrivateSubnetsOutput-2',
+        });
+        new cdk.CfnOutput(this, 'AthenaVpcPublicSubnetsOutput', {
+            // value: JSON.stringify(vpc.privateSubnets.map(subnet => subnet.subnetId)),
+            value: vpc.publicSubnets.map(subnet => subnet.subnetId).join(','),
+            exportName: 'AthenaVpcPublicSubnetsOutput-2',
         });
         new cdk.CfnOutput(this, 'AthenaVpcAvailabilityZonesOutput', {
             // value: JSON.stringify(vpc.availabilityZones),
